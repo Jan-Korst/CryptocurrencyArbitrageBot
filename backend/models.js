b26251a3b1a7598b719e583c8b86178943c33ab2
Jan-Korst <54290818+Jan-Korst@users.noreply.github.com>
2024-07-10 08:07:08 +0300

    cleaning code

--- backend/models.js
+++ backend/models.js
@@ -2,24 +2,24 @@ const fs = require('fs');
 const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
+
 class Exchange {
   constructor() {
     this.filePath = path.join(__dirname, 'exchangeInfo.json');
   }
-  saveExchangeInfo(name, apiKey, secret) {
-    const exchangeInfo = {
-      name,
-      apiKey: apiKey ? apiKey : process.env.API_KEY, 
-      secret: secret ? secret : process.env.SECRET,
-    };
+
+  saveExchangeInfo(name, apiKey = process.env.API_KEY, secret = process.env.SECRET) {
+    const exchangeInfo = { name, apiKey, secret };
     fs.writeFileSync(this.filePath, JSON.stringify(exchangeInfo, null, 2), 'utf8');
   }
+
   getExchangeInfo() {
-    if (fs.existsSync(this.filePath)) {
-      const data = fs.readFileSync(this.filePath, 'utf8');
-      return JSON.parse(data);
+    try {
+      return JSON.parse(fs.readFileSync(this.filePath, 'utf8'));
+    } catch (e) {
+      return null;
     }
-    return null;
   }
 }
+
 module.exports = Exchange;
